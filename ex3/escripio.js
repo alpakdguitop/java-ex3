ba5ba541257e40ff806ea9a5cb0ef2bf2ca14da9
alpakdguitop <93211603+alpakdguitop@users.noreply.github.com>
2021-12-10 13:51:00 +0100

    Add files via upload

---
+++ ex3/escripio.js
@@ -0,0 +1,21 @@
+/*
+	Exemplo de javascript
+*/
+alert("Sal de aquí corriendo");
+  var moito = 9 ;
+    var raiz = Math.sqrt(moito);
+    alert(raiz);
+    var pefro = 4;
+    var cuadrado = Math.log(pefro);
+    alert(cuadrado);
+    var was = 5.25;
+    var wes = 4;
+    var tres = Math.pow(was,wes);
+    alert(tres);
+
+    alert(Math.round(Math.PI));
+
+    alert(Math.min(moito,was,wes));
+
+     function getRandom() {}
+    alert(Math.round(Math.random(0,3)));
